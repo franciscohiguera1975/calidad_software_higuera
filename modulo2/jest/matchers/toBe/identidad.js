07b613990cb6be4d261ef35d4913e45c1d7b77fe
Francisco Higuera <prof.francisco.higuera@ute.edu.ec>
2026-06-08 07:54:05 -0500

    clase 08-06-2026

---
+++ modulo2/jest/matchers/toBe/identidad.js
@@ -0,0 +1,16 @@
+function crearAlumno(valor, { clone = false } = {}) {
+    const esObjeto = valor !== null && typeof valor === 'object';
+    if (clone) {
+        if (!esObjeto) throw new TypeError('Solo se puede clonar un objeto');
+        if (Array.isArray(valor)) return [...valor];
+        return { ...valor }
+    }
+    return valor;
+}
+function suma(a,b){
+    if(typeof a!=='number' || typeof b!=='number'){
+        throw new TypeError('a y b deben ser números');
+    }
+    return a+b;
+}
+module.exports = { identidad, suma };
